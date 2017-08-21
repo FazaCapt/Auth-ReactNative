438ce543a722256d0ded327f4c2a8b5419b802ca
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-21 11:39:27 +0700

    Login Form Scaffolding

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './component/common';
+import { Header } from './components/common';
+import  LoginForm  from './components/LoginForm';
+
+
 
 
 class App extends Component {
@@ -21,7 +24,7 @@ class App extends Component {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App!</Text>
+                <LoginForm />
             </View>
 
         );
